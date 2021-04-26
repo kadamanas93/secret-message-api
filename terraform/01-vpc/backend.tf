6a447fc14e29e7cb7742d1e2ca0327ba5c8e8736
Anas Kadam <anaskadam@gmail.com>
2021-04-26 10:27:14 -0400

    Enhance vpc module outputs

--- terraform/01-vpc/backend.tf
+++ terraform/01-vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backend"
+    bucket = "terraform-s3-backend-mine"
     key    = "vpc.tfstate"
     region = "us-east-2"
   }
--- terraform/01-vpc/outputs.tf
+++ terraform/01-vpc/outputs.tf
@@ -1,8 +1,14 @@
 output "vpc" {
   value = {
-    id              = module.vpc.vpc_id
-    cidr_block      = module.vpc.vpc_cidr_block
-    private_subnets = module.vpc.private_subnets
-    public_subnets  = module.vpc.public_subnets
+    id         = module.vpc.vpc_id
+    cidr_block = module.vpc.vpc_cidr_block
+    private_subnets = {
+      ids   = module.vpc.private_subnets
+      cidrs = module.vpc.private_subnets_cidr_blocks
+    }
+    public_subnets = {
+      ids   = module.vpc.public_subnets
+      cidrs = module.vpc.public_subnets_cidr_blocks
+    }
   }
 }
